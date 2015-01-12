32dd879e9e1bc8612d2a58aa7651d7d2e9e96b82
balasankar- <balasankar@outlook.com>
2015-01-12 17:01:28 +0530

    JSON on button click works now

---
+++ IgniteUI/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace IgniteUI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-2.13.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+
+            bundles.Add(new StyleBundle("~/Contents/css").Include(
+                      "~/Contents/bootstrap.min.css"));
+        }
+    }
+}
